84a100dd9f8e8643a3ca4b6de1f89d9b7addb05e
Linus Jäger <34212061+linus307@users.noreply.github.com>
2021-02-20 09:50:51 +0100

    Update and rename x730.sh to x735.sh

--- x735.sh
+++ x735.sh
@@ -1,9 +1,7 @@
-#x730 Powering on /reboot /shutdown from hardware
+#x735 Powering on /reboot /shutdown from hardware
 #!/bin/bash
 
-    sudo sed -e '/shutdown/ s/^#*/#/' -i /etc/rc.local
-
-    echo '#!/bin/bash
+sudo bash -c "echo '#!/bin/bash
 
 SHUTDOWN=4
 REBOOTPULSEMINIMUM=200
@@ -15,7 +13,7 @@ echo "$BOOT" > /sys/class/gpio/export
 echo "out" > /sys/class/gpio/gpio$BOOT/direction
 echo "1" > /sys/class/gpio/gpio$BOOT/value
 
-echo "X730 Shutting down..."
+echo "X735 Shutting down..."
 
 while [ 1 ]; do
   shutdownSignal=$(cat /sys/class/gpio/gpio$SHUTDOWN/value)
@@ -38,17 +36,13 @@ while [ 1 ]; do
       exit
     fi
   fi
-done' > /etc/x730pwr.sh
-sudo chmod +x /etc/x730pwr.sh
-sudo sed -i '$ i /etc/x730pwr.sh &' /etc/rc.local 
+done' > /usr/local/bin/x735pwr.sh"
+sudo chmod +x /etc/x735pwr.sh
 
 
-#X730 full shutdown through Software
-#!/bin/bash
+#X735 full shutdown through Software
 
-    sudo sed -e '/button/ s/^#*/#/' -i /etc/rc.local
-
-    echo '#!/bin/bash
+sudo bash -c "echo '#!/bin/bash
 
 BUTTON=18
 
@@ -63,10 +57,25 @@ if ! [[ $SLEEP =~ $re ]] ; then
    echo "error: sleep time not a number" >&2; exit 1
 fi
 
-echo "X730 Shutting down..."
+echo "X735 Shutting down..."
 /bin/sleep $SLEEP
 
 #restore GPIO 18
-echo "0" > /sys/class/gpio/gpio$BUTTON/value
-' > /usr/local/bin/x730shutdown.sh
-sudo chmod +x /usr/local/bin/x730shutdown.sh
+echo "0" > /sys/class/gpio/gpio$BUTTON/value' > /usr/local/bin/x735shutdown.sh"
+sudo chmod +x /usr/local/bin/x735shutdown.sh
+
+
+#X735 Systemd service
+
+sudo bash -c "echo '[Unit]
+Description=Startup Script
+After=reboot.target
+
+[Service]
+Type=simple
+ExecStart=/bin/bash /usr/local/sbin/x735pwr.sh
+
+[Install]
+WantedBy=multi-user.target' > /etc/systemd/system/shutdown_x735.service"
+
+sudo systemctl enable shutdown_x735
